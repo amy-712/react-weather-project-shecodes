a653b760317de94ebcc2835abfefe5087f9c57be
amy-712 <91413690+amy-712@users.noreply.github.com>
2022-02-05 16:56:57 +0000

    installed axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,69 @@
-import React from "react"
-import './weather.css'
+import React, { useState } from "react";
+import "./weather.css";
+import axios from "axios";
 
-export default function Weather(){
-    return <div className="Weather">
-        <div className="container">
-            
-            <form>
-                <div className="row gx-0">
-                <div className="col-4">
- <div className="mb-3">
-                <input type="text" placeholder="🕵🏼‍♀️Search for City" autoFocus="on"></input>
-                </div>
-                </div>
-                <div className="col-4">
-                    <button type="submit" class="btn btn-primary">Search</button>
-                </div>
-                <div className="col-4">
-                    <button type="submit" class="btn btn-success" id="my-location-button">Use My Location</button>
-                </div>
-                </div>
-            </form>
-            <div className="row">
-                <div className="col-4">
-                <img className="today-emoji" src="http://openweathermap.org/img/wn/04n@2x.png" alt="broken clouds"/>
-                    <h1>5<span className="temperatureUnit">
-                    ℃</span></h1>
-                </div>
-                <div className="col-4">
-                    <ul className="weatherType">
-                        <li>Humidity: 17%</li>
-                        <li>Wind: 5mph</li>
-                    </ul>
-                </div>
-                <div className="col-4">
-                    <ul className="location">
-                        <h2>London</h2>
-                        <li>
-                            Thursday 20:50
-                        </li>
-                        <li>Partly Cloudy</li>
-                    </ul>
-                </div>
+export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  const apiKey = "2236c7e355f68293390c37c68fbd7525";
+  let city = "London";
+  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  return (
+    <div className="Weather">
+      <div className="container">
+        <form>
+          <div className="row gx-0">
+            <div className="col-4">
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="🕵🏼‍♀️Search for City"
+                  autoFocus="on"
+                ></input>
+              </div>
             </div>
+            <div className="col-4">
+              <button type="submit" class="btn btn-primary">
+                Search
+              </button>
             </div>
-        
+            <div className="col-4">
+              <button
+                type="submit"
+                class="btn btn-success"
+                id="my-location-button"
+              >
+                Use My Location
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="row">
+          <div className="col-4">
+            <img
+              className="today-emoji"
+              src="http://openweathermap.org/img/wn/04n@2x.png"
+              alt="broken clouds"
+            />
+            <h1>
+              5<span className="temperatureUnit">℃</span>
+            </h1>
+          </div>
+          <div className="col-4">
+            <ul className="weatherType">
+              <li>Humidity: 17%</li>
+              <li>Wind: 5mph</li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <ul className="location">
+              <h2>London</h2>
+              <li>Thursday 20:50</li>
+              <li>Partly Cloudy</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
