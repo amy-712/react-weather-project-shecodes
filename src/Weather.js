b285f17bff31a55c2e54275854c06585522537e0
amy-712 <91413690+amy-712@users.noreply.github.com>
2022-02-05 16:15:00 +0000

    Added webpage styling

--- src/Weather.js
+++ src/Weather.js
@@ -6,31 +6,34 @@ export default function Weather(){
         <div className="container">
             
             <form>
-                <div className="row">
+                <div className="row gx-0">
                 <div className="col-4">
- 
-                <input type="text" placeholder="🕵🏼‍♀️Search for City"></input>
+ <div className="mb-3">
+                <input type="text" placeholder="🕵🏼‍♀️Search for City" autoFocus="on"></input>
+                </div>
                 </div>
                 <div className="col-4">
                     <button type="submit" class="btn btn-primary">Search</button>
                 </div>
                 <div className="col-4">
-                    <button type="submit" class="btn btn-success">Use My Location</button>
+                    <button type="submit" class="btn btn-success" id="my-location-button">Use My Location</button>
                 </div>
                 </div>
             </form>
             <div className="row">
                 <div className="col-4">
-                    <h2>🌤 5℃</h2>
+                <img className="today-emoji" src="http://openweathermap.org/img/wn/04n@2x.png" alt="broken clouds"/>
+                    <h1>5<span className="temperatureUnit">
+                    ℃</span></h1>
                 </div>
                 <div className="col-4">
-                    <ul>
+                    <ul className="weatherType">
                         <li>Humidity: 17%</li>
                         <li>Wind: 5mph</li>
                     </ul>
                 </div>
                 <div className="col-4">
-                    <ul>
+                    <ul className="location">
                         <h2>London</h2>
                         <li>
                             Thursday 20:50
